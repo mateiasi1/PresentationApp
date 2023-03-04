63dfb96e767b1bec3cd85609d95c23ba30f0ca42
Dragos Mateiasi <dragosdm22@gmail.com>
2023-03-04 23:09:44 +0200

    add email button animation

--- app/components/home/email/emailButton.tsx
+++ app/components/home/email/emailButton.tsx
@@ -1,7 +1,7 @@
 const EmailButton = () => {
     return (
         <>
-            <button className="bg-customTurquiose text-buttonText text-white font-bold py-3 px-5 border border-blue-700 rounded-[4px] border-customTurquiose">
+            <button className="bg-customTurquiose text-buttonText text-white font-bold py-3 px-5 border border-blue-700 rounded-[4px] border-customTurquiose bg-blue-600 text-white outline-none focus:ring-4 shadow-lg transform active:scale-y-75 transition-transform flex">
                 Contact me
             </button>
         </>
