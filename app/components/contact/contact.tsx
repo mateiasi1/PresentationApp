452686b49ecbba3e34d36052f354f8f9a1878e4a
Dragos Mateiasi <dragosdm22@gmail.com>
2023-03-05 18:12:43 +0200

    add contact page it2

--- app/components/contact/contact.tsx
+++ app/components/contact/contact.tsx
@@ -1,7 +1,14 @@
 import ContactForm from "./contactForm";
 
 const ContactPage = () => {
-    return <ContactForm />;
+    return (
+        <div>
+            <h1 className="text-center pt-5 font-bold md:text-lg lg:text-xl ">
+                Send me a message
+            </h1>
+            <ContactForm />
+        </div>
+    );
 };
 
 export default ContactPage;
--- app/components/contact/contactForm.tsx
+++ app/components/contact/contactForm.tsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+
 const ContactForm = () => {
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const handleSubjectChange = (event: any) => {
+        setSubject(event.target.value);
+    };
+
+    const handleMessageChange = (event: any) => {
+        setMessage(event.target.value);
+    };
+    const handleNameChange = (event: any) => {
+        setName(event.target.value);
+    };
+    const handleEmailChange = (event: any) => {
+        setEmail(event.target.value);
+    };
+    const handleSubmit = async (event: any) => {};
+
     return (
         <div className="flex flex-row justify-center">
             <div className="max-w-sm rounded overflow-hidden shadow-lg p-5 w-3/4">
@@ -7,15 +28,17 @@ const ContactForm = () => {
                         <div className="w-full px-3">
                             <label
                                 className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                htmlFor="grid-first-name"
+                                htmlFor="grid-name"
                             >
                                 Name
                             </label>
                             <input
                                 className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                                id="grid-first-name"
+                                id="grid-name"
                                 type="text"
                                 placeholder="Jane"
+                                required
+                                onChange={handleNameChange}
                             />
                             <p className="text-red-500 text-xs italic">
                                 Please fill out this field.
@@ -26,15 +49,18 @@ const ContactForm = () => {
                         <div className="w-full px-3">
                             <label
                                 className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                htmlFor="grid-password"
+                                htmlFor="grid-email"
                             >
                                 Email
                             </label>
                             <input
                                 className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                id="grid-password"
-                                type="text"
+                                id="grid-email"
+                                type="email"
+                                autoComplete="email"
                                 placeholder="example@email.com"
+                                required
+                                onChange={handleEmailChange}
                             />
                             <p className="text-gray-600 text-xs italic">
                                 Review the email and assure it's in a correct
@@ -55,6 +81,8 @@ const ContactForm = () => {
                                 id="grid-password"
                                 type="text"
                                 placeholder="Frontend project collaboration"
+                                required
+                                onChange={handleSubjectChange}
                             />
                         </div>
                     </div>
@@ -62,14 +90,16 @@ const ContactForm = () => {
                         <div className="w-full px-3">
                             <label
                                 className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                htmlFor="grid-password"
+                                htmlFor="grid-message"
                             >
                                 Message
                             </label>
                             <textarea
                                 className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                id="grid-password"
+                                id="grid-message"
                                 placeholder="Message"
+                                required
+                                onChange={handleMessageChange}
                             />
                             <p className="text-gray-600 text-xs italic">
                                 Make it as long and as crazy as you'd like
@@ -80,7 +110,10 @@ const ContactForm = () => {
                         By clicking below, you are agreeing to share the above
                         details with Dragoș Mateiași.
                     </p>
-                    <button className="bg-customTurquiose text-buttonText text-white font-bold py-3 px-5 border border-blue-700 rounded-[4px] border-customTurquiose bg-blue-600 text-white outline-none focus:ring-4 shadow-lg transform active:scale-y-75 transition-transform flex w-full justify-center">
+                    <button
+                        type="submit"
+                        className="bg-customTurquiose text-buttonText text-white font-bold py-3 px-5 border border-blue-700 rounded-[4px] border-customTurquiose bg-blue-600 text-white outline-none focus:ring-4 shadow-lg transform active:scale-y-75 transition-transform flex w-full justify-center"
+                    >
                         Send message
                     </button>
                 </form>
