db32db333b26744c44663d276e09d2cc45ec8e7a
Dragos Mateiasi <dragosdm22@gmail.com>
2023-03-06 00:10:15 +0200

    add clip-path

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -10,13 +10,23 @@ export default function Index() {
         // <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
         <div>
             <Layout>
-                <div className="bg-gradient-to-r from-gradientBgFrom to-gradientBgTo">
+                <div className="bg-gradient-to-r from-gradientBgFrom to-gradientBgTo h-[350px]">
                     <div className=" flex flex-row justify-around min-h-screen py-10 px-10">
                         <div className="h-full w-2/4">
                             <PresentationText />
+                        </div>
+                        <img
+                            src={devImage}
+                            alt="..."
+                            className="relative z-10"
+                        />
+                    </div>
+                </div>
+                <div className="bg-gradient-to-r from-gradientBgFrom clip-path-home to-gradientBgTo h-[250px]">
+                    <div className=" flex flex-row justify-around min-h-screen py-10 px-10">
+                        <div className="h-full w-2/4">
                             <EmailButton />
                         </div>
-                        <img src={devImage} alt="..." className="" />
                     </div>
                 </div>
             </Layout>
