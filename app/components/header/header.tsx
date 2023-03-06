d1698ef2021fba27922ec48a565fe19d684b8101
Dragos Mateiasi <dragosdm22@gmail.com>
2023-03-06 18:35:04 +0200

    add logo

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,21 +1,14 @@
 import AboutMe from "./aboutMe";
-
+import logo from "../../../public/images/logo.png";
 const Header = () => {
     return (
         <nav className="relative flex items-center justify-between flex-wrap text-[color:white] bg-grayColor p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
                 <a href="/" className="flex flex-row items-center">
-                    <svg
-                        className="fill-current h-8 w-8 mr-2"
-                        width="54"
-                        height="54"
-                        viewBox="0 0 54 54"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-                    </svg>
+                    <img src={logo} className="h-12 w-12" />
+
                     <span className="font-semibold text-xl tracking-tight">
-                        Tailwind CSS
+                        Dragoș Mateiași
                     </span>
                 </a>
             </div>
